2540014c02c33d34a1743b5c8013d4ac0a04dfb5
German Uriarte <uriarte.german@gmail.com>
2024-09-16 16:32:12 -0400

    added palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let string = str.toLowerCase().replace(/\W/gi, "");
+  let isPalindrome = true;
 
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.charAt(i) != string.charAt(string.length - i - 1)) {
+      console.log(string.charAt(i), string.charAt(string.length - i));
+
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
