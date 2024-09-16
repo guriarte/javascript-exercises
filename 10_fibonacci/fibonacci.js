e1964cc6ba558dee79a35e1050ca81525b7d2925
German Uriarte <uriarte.german@gmail.com>
2024-09-16 17:35:11 -0400

    added fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const number = Number.parseInt(num);
+  let fibonacci = 0;
+  const lastTwoNumbers = [0, 1];
+  if (number < 0) return "OOPS";
+  if (number === 0) return 0;
+  if (number === 1) return 1;
 
+  for (let i = 2; i <= num; i++) {
+    fibonacci = lastTwoNumbers[0] + lastTwoNumbers[1];
+    lastTwoNumbers.shift();
+    lastTwoNumbers.push(fibonacci);
+  }
+  return fibonacci;
 };
 
 // Do not edit below this line
