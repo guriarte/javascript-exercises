9349f6f1885fd05bbc26f54a8cb6e7dac53c38c5
German Uriarte <uriarte.german@gmail.com>
2024-09-17 10:53:04 -0400

    added findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  arr.forEach((element) => {
+    if (!element.yearOfDeath) element.yearOfDeath = 2024;
+  });
+  const sortedArray = arr.sort((personA, personB) => {
+    const personAAge = personA.yearOfDeath - personA.yearOfBirth;
+    const personBAge = personB.yearOfDeath - personB.yearOfBirth;
+    return personAAge > personBAge ? -1 : 1;
+  });
+  return sortedArray[0];
 };
 
 // Do not edit below this line
